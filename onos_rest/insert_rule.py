8935381d35fb6dba7dc0c43f9d21a26cea914d8f
Manoel <manoelrmiranda@gmail.com>
2017-09-22 13:11:31 -0300

    Add rule

---
+++ onos_rest/insert_rule.py
@@ -0,0 +1,56 @@
+import pycurl
+import json
+
+try:
+    # python 3
+    from urllib.parse import urlencode
+except ImportError:
+    # python 2
+    from urllib import urlencode
+
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://localhost:8181/onos/v1/docs/flows/{deviceId}')
+json_rule = {
+	  "priority": 400000,
+	  "timeout": 0,
+	  "isPermanent": True,
+	  "deviceId": "of:0000000000000001",
+	  "treatment": {
+	    "instructions": [
+	      {
+	        "type": "OUTPUT",
+	        "port": "CONTROLLER"
+	      }
+	    ]
+	  },
+	  "selector": {
+	    "criteria": [
+	      {
+	        "type": "ETH_TYPE",
+	        "ethType": "0x88cc"
+	      },
+	      {
+	        "type": "ETH_DST",
+	        "mac": "00:00:11:00:00:01"
+	      },
+	      {
+	        "type": "ETH_SRC",
+	        "mac": "00:00:11:00:00:01"
+	      }
+	    ]
+	  }
+	}
+#post_data = {'field': 'value'}
+#post_data = json.loads(json_rule)
+post_data = json_rule
+# Form data must be provided already urlencoded.
+postfields = urlencode(post_data)
+# Sets request method to POST,
+# Content-Type header to application/x-www-form-urlencoded
+# and data to send in request body.
+c.setopt(c.POSTFIELDS, postfields)
+#c.setopt(pycurl.PROXYUSERPWD, str("%s:%s" % (proxy["username"], proxy["password"])))
+c.setopt(pycurl.PROXYUSERPWD, "onos:rocks")
+
+c.perform()
+c.close()
